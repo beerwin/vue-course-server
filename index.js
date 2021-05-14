1b89ed3fac344794179b13a0908940e6f7d6543e
Nagy Ervin <nagyervin.bws@gmail.com>
2021-05-14 23:19:58 +0300

    added cors support

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 const api = require('./app/routes/api');
+app.use(cors({
+   origin: '*',
+   optionsSuccessStatus: 200
+}));
 app.use(express.json());
 app.use('/api', api);
 
